35cce71a7a82456e214a935c12cdcc46eb049fe3
kai815 <h.kaizuka815@gmail.com>
2020-06-07 16:38:38 +0900

    型の定義とデータ、コンポーネントの作成

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface IList {
+  readonly id: number; //idは途中で変更しないのでreadonly。
+  name: string; // 文字列なのでstring型
+  cards: ICard[]; // 配列を定義するには 要素[] とする
+}
+
+export interface ICard {
+  readonly id: number; //idは途中で変更しないのでreadonly。
+  text: string; // 文字列なのでstring型
+}
